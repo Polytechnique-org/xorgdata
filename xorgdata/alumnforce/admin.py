1b51fa104b394b9baece8dc3469079fcf47579a3
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2018-12-04 23:29:18 +0100

    Bootstrap an admin.py

--- xorgdata/alumnforce/admin.py
+++ xorgdata/alumnforce/admin.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Polytechnique.org
+# This code is distributed under the Affero General Public License version 3
+
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.Account)
+class AccountAdmin(admin.ModelAdmin):
+    search_fields = ['hrid', 'main_email', 'fullname', 'preferred_name']
+
+    list_display = ['af_id', 'ax_id', 'xorg_id', 'first_name', 'last_name']
+    ordering = ('-ax_id', 'xorg_id', 'af_id')
