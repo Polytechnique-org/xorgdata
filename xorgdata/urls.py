25b028ad5215c8b60e46bdd37a863959e47d50e6
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2019-04-22 23:14:29 +0200

    Add robots.txt
    Disallow indexing of the website/API by anyone

--- xorgdata/urls.py
+++ xorgdata/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
