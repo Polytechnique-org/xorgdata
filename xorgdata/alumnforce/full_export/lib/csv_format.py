7b56f313b4cebc7318c18818fc8102ba3c0aa0ef
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2019-11-09 16:13:14 +0100

    Add new fields from AlumnForce's full export

--- xorgdata/alumnforce/full_export/lib/csv_format.py
+++ xorgdata/alumnforce/full_export/lib/csv_format.py
@@ -83,6 +83,8 @@ ALUMNFORCE_FIELDS = (
     ('Nom d\'usage', 'usage_name', None),
     ('Civilité (Mme / Mlle / M.)', 'civility', None),
     ('Date de naissance', 'birth_date', None),
+    ('Date de création de compte', 'account_creation_date', None),
+    ('Date de validation des CGU', 'cgu_validation_date', None),
     ('Adresse personnelle - Ligne 1', 'personal.address.line_1', None),
     ('Adresse personnelle - Ligne 2', 'personal.address.line_2', None),
     ('Adresse personnelle - Ligne 3', 'personal.address.line_3', None),
